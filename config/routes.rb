e5cebed4e48c969de472a341cc4e9dbe82963ef4
Erfaneh Gharavi <erfaneh_gharavi@yahoo.com>
2014-06-29 13:06:28 +0430

    F2

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,20 @@ Textmining::Application.routes.draw do
 #match 'controller/upload', to: '#upload',    via: 'post'
 #match '/upload',    to: 'static_pages#uploaded',    via: 'get' 
 
-# resources :users
+ resources :users
 root  'static_pages#index'
-#match '/', to: 'static_pages#home', via: 'get'
-#match '/help',    to: 'static_pages#help',    via: 'get'
-#match '/about',   to: 'static_pages#about',   via: 'get'
-#match '/contact', to: 'static_pages#contact', via: 'get'
-#match '/upload',   to: 'static_pages#upload',   via: 'get'
-#match '/uploaded',   to: 'static_pages#uploaded',   via: 'get'
+match '/', to: 'static_pages#home', via: 'get'
+match '/help',    to: 'static_pages#help',    via: 'get'
+match '/about',   to: 'static_pages#about',   via: 'get'
+match '/contact', to: 'static_pages#contact', via: 'get'
+match '/upload',   to: 'static_pages#upload',   via: 'get'
+match '/uploaded',   to: 'static_pages#uploaded',   via: 'get'
 
-#match '/uploaded',   to: 'static_pages#uploaded',   via: 'post'
+match '/uploaded',   to: 'static_pages#uploaded',   via: 'post'
 
-#match '/inputtext',   to: 'static_pages#inputtext',   via: 'get'
+match '/inputtext',   to: 'static_pages#inputtext',   via: 'get'
 
-#match '/score', to: 'static_pages#score', via: 'get'
+match '/score', to: 'static_pages#score', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
