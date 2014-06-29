f07a27d978b59518a9504cd7d923e9f3ccff7566
Erfaneh Gharavi <erfaneh_gharavi@yahoo.com>
2014-06-29 17:12:53 +0430

    F

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628214357) do
+ActiveRecord::Schema.define(version: 20140629123936) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "firstarticle"
-    t.string   "secondarticle"
+    t.text     "firstarticle"
+    t.text     "secondarticle"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
