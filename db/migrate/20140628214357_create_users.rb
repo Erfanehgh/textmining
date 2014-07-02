b4f1543b904496282ed468d85f36ef199bd21b62
Erfaneh Gharavi <erfaneh_gharavi@yahoo.com>
2014-07-03 03:41:53 +0430

    DB

--- db/migrate/20140628214357_create_users.rb
+++ db/migrate/20140628214357_create_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :firstarticle
-      t.string :secondarticle
+      t.text :firstarticle
+      t.text :secondarticle
 
       t.timestamps
     end
